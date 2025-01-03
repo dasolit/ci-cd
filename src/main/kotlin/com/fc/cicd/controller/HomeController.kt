f14b041219ec9a1bab2c6e5e8fc37ce76d2c7fb4
itmdeveloper <choidasol95@gmail.com>
2025-01-04 02:07:26 +0900

    테스트 용 프로젝트 생성

---
+++ src/main/kotlin/com/fc/cicd/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.fc.cicd.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("")
+    fun home(): String{
+        return "Hello World";
+    }
+}
